4b0729e986d9662f3221cc2a44a9034425ec1583
fernandocomet <fernandocomet@gmail.com>
2020-06-04 13:16:29 +0200

    adding stuff

---
+++ build/precache-manifest.59512707f349717fadfbfddd0bc25702.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd72944584052329a82609dca83923d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f28a18e650b4c7a56c7",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e11ce63b291141b9b875",
+    "url": "/static/css/main.2d5baf8e.chunk.css"
+  },
+  {
+    "revision": "1f28a18e650b4c7a56c7",
+    "url": "/static/js/2.02a609b6.chunk.js"
+  },
+  {
+    "revision": "b9b22731cdb097fe33255351723dd2eb",
+    "url": "/static/js/2.02a609b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e11ce63b291141b9b875",
+    "url": "/static/js/main.f4cee51a.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "d579732e4c0602d4bb3b5a7f9d246516",
+    "url": "/static/media/white_dev-dot-to.d579732e.svg"
+  },
+  {
+    "revision": "b0ffda7df03eaf0fdd257dce1635c8b6",
+    "url": "/static/media/white_github.b0ffda7d.svg"
+  },
+  {
+    "revision": "c2b14474966237465c0b4c466ec31c85",
+    "url": "/static/media/white_home.c2b14474.svg"
+  },
+  {
+    "revision": "5ad00d9c24a0479c5ac50681934d9256",
+    "url": "/static/media/white_instagram.5ad00d9c.svg"
+  },
+  {
+    "revision": "45868efe29db07002b5ffb8858954107",
+    "url": "/static/media/white_linkedin.45868efe.svg"
+  },
+  {
+    "revision": "aec6844e0e04fa4bfcba6d14cf4c27fa",
+    "url": "/static/media/white_medium.aec6844e.svg"
+  },
+  {
+    "revision": "7c7cf9f8d20df8c80ca990eca98fa30b",
+    "url": "/static/media/white_monster.7c7cf9f8.svg"
+  },
+  {
+    "revision": "3bb7ebde7bfbef0e52cbce3abafc53c7",
+    "url": "/static/media/white_pinterest.3bb7ebde.svg"
+  },
+  {
+    "revision": "19dbfe99c4ab2caa21ee6c93ad6544f4",
+    "url": "/static/media/white_text.19dbfe99.svg"
+  },
+  {
+    "revision": "b09cfa83e6110a75bd719a5a34a84f71",
+    "url": "/static/media/white_twitter.b09cfa83.svg"
+  },
+  {
+    "revision": "9308fc71af4c3bc86319061acbf41ed7",
+    "url": "/static/media/white_workroll.9308fc71.svg"
+  },
+  {
+    "revision": "008eed337aa61b7f6adb93882b4eafe9",
+    "url": "/static/media/white_works.008eed33.svg"
+  }
+]);
