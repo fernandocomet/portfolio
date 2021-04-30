d63047b2acbf6903637009d5cb5261792a7a87a3
Fernando Comet <fernandocomet@gmail.com>
2021-04-30 12:50:03 +0200

    change footer links

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ import github from "./img/social/white_github.svg";
 import medium from "./img/social/white_medium.svg";
 import twitter from "./img/social/white_twitter.svg";
 import instagram from "./img/social/white_instagram.svg";
-import devto from "./img/social/white_dev-dot-to.svg";
+import webflow from "./img/social/white_webflow.svg";
 import pinterest from "./img/social/white_pinterest.svg";
 import './Footer.css';
 
@@ -16,9 +16,9 @@ class Social extends Component{
         social:[
           {name: "Github", src: github, url:"https://github.com/fernandocomet"},
           {name: "Medium", src: medium, url:"https://medium.com/@fernandocomet"},
+          {name: "Webflow", src: webflow, url:"https://webflow.com/fernandocomet"},
           {name: "Linkedin", src: linkedin, url:"https://www.linkedin.com/in/fernandocomet/"},
           {name: "Twitter", src: twitter, url:"https://twitter.com/fernandocomet"},
-          {name: "DevTo", src: devto, url:"https://dev.to/fernandocomet"},
           {name: "Instagram", src: instagram, url:"https://www.instagram.com/fernandocomet/"},
           {name: "Pinterest", src: pinterest, url:"https://www.pinterest.es/fernandocomet/"}
         ]
